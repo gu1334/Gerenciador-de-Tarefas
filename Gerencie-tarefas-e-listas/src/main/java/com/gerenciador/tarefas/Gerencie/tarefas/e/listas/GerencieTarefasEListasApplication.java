0ff583bb3f705177746747fccd1d363fd9806531
gu1334 <freiregustavo.c@gmail.com>
2025-01-07 18:52:21 -0300

    Primeiro commit

---
+++ Gerencie-tarefas-e-listas/src/main/java/com/gerenciador/tarefas/Gerencie/tarefas/e/listas/GerencieTarefasEListasApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciador.tarefas.Gerencie.tarefas.e.listas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerencieTarefasEListasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerencieTarefasEListasApplication.class, args);
+	}
+
+}
---
+++ Gerencie-tarefas-e-listas/src/test/java/com/gerenciador/tarefas/Gerencie/tarefas/e/listas/GerencieTarefasEListasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciador.tarefas.Gerencie.tarefas.e.listas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerencieTarefasEListasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
